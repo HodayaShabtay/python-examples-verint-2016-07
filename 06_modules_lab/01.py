a87fa72f0cd8704aa53455134b9098fc36392110
HodayaShabtay <hodush12@gmail.com>
2016-07-07 23:05:50 +0300

    Solution for: sol-python-verint.modules-lab/01.py

--- 06_modules_lab/01.py
+++ 06_modules_lab/01.py
@@ -1,4 +1,14 @@
-""" Write a program that takes a count
-from sys.argv import and prints "Hello Python"
-count times
-"""
+
+import sys
+
+if len(sys.argv) < 2:
+	print "Error, please send number as argument"
+	sys.exit()
+	
+if sys.argv[1].isdigit() == False:
+	print "Strings are not valid as argument, only numbers"
+	sys.exit()
+	
+for i in range(int(sys.argv[1])):
+	print "Hello\n"
+	
