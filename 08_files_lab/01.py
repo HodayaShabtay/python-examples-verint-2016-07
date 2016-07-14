cedde4524b60a694056010d34a646d3090569987
HodayaShabtay <hodush12@gmail.com>
2016-07-14 20:52:54 +0300

    Solution for: sol-python-verint.files-lab/01.py

--- 08_files_lab/01.py
+++ 08_files_lab/01.py
@@ -1,6 +1,17 @@
-"""
-Write a program that takes 2 file names
-and appends the second file's contents to
-the end of the first one
-"""
 
+import sys
+import os
+
+if len(sys.argv) < 3:
+	sys.exit("Error, please insert source and destination")
+	
+if not os.path.isfile(sys.argv[1]) or not os.path.isfile(sys.argv[2]):
+	sys.exit("Error, please insert valid source and destination path")
+
+source, dest = sys.argv[1:]
+	
+with open(source, 'r') as fin:
+	with open(dest,'a') as fout:
+		for line in fin:
+			fout.write("\n {0}".format(line))
+	
