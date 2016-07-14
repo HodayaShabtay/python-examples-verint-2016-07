59a058f7b8907cfff72c16f174ebb75e1beb1c2b
HodayaShabtay <hodush12@gmail.com>
2016-07-14 20:53:16 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,33 @@
 """
-Write a program that prints only unique values
-from sys.argv (without program name)
+This program prints all anagrams in a given file
 """
+
+
+import sys
+import os
+import argparse
+import collections
+
+parser = argparse.ArgumentParser(description='This program print all anagrams in a given file')
+parser.add_argument(help='Valid path to words file',dest='file')
+args = parser.parse_args()
+
+if not os.path.isfile(args.file):
+	sys.exit("file {0} doesn't exist".format(args.file))
+	
+anagrams = collections.defaultdict(list)	
+with open(args.file) as words:
+	for line in words:
+		for word in line.split(' '):
+			anagrams[tuple((sorted(filter(lambda a: a != '\n', word))))].append(word.rstrip())
+			
+for words in anagrams.values():
+	for word in words:
+		sys.stdout.write(" {0}".format(word))
+	sys.stdout.write('\n')
+
+	
+			
+	
+
 
