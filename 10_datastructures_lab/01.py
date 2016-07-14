c8dcd8381bd7b6720fe19b6649cb93032f396d15
HodayaShabtay <hodush12@gmail.com>
2016-07-14 20:53:15 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,10 +1,29 @@
-"""
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
-"""
+
+import sys
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='This program retrive IP for hostname')
+parser.add_argument(help='IP addresses file',dest='file')
+parser.add_argument(nargs='+',help='hostnames list',dest='hosts')
+
+args = parser.parse_args()
+
+if not os.path.isfile(args.file):
+	sys.exit("error! file {0} doesn't exist".format(args.file))
+
+dic = {}
+with open(args.file) as file:
+	for line in file:
+		dic[line.split('=')[0]] = line.split('=')[1]
+
+for item in args.hosts:
+	if item in dic:
+		print dic[item]
+	else:
+		print "hostname '{0}' was not found".format(item)
+		
+
+
+
+
