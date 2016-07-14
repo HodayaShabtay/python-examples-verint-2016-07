4cf34faea46d751788c9d2b2fe4f846c30c0d3c1
HodayaShabtay <hodush12@gmail.com>
2016-07-14 21:14:30 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,11 @@
+
 """
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
+This program prints all possible permutation of 3 small letters
 """
 
+import sys
+
+letters = [chr(i) for i in range(97,123)]
+words = ["".join(a+b) for a in letters for b in letters]
+groups = ["".join(a+b) for a in words for b in letters]
+print groups
