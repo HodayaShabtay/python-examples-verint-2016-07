f138a57274b32f3c25d8cac389204f31382b6958
HodayaShabtay <hodush12@gmail.com>
2016-07-15 18:19:47 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,14 @@
-"""
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
-"""
 
+import sys
+import os
+
+def calculate(*nums):
+	try:
+		return sum([n%100/10 for n in nums])
+	except:
+		print "{0}, {1}".format(sys.exc_info()[0],sys.exc_info()[1])
+	
+	
+print calculate(1120, 220, 140)
+print calculate('hodaya', 400,800)
+print calculate(900, 400,800)
