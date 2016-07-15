6082d929a92c7cd74c414359addd43115dd80a2b
HodayaShabtay <hodush12@gmail.com>
2016-07-15 18:19:46 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,27 @@
-"""
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
-"""
+
+import sys
+import os
+import argparse
+
+def except_for_string_int(val1, val2):
+	try:
+		if val1.isdigit():
+			if val2.isdigit():
+				raise ValueError('excepts for integer and string, received tow integers')
+			else:
+				print "Valid input"
+		elif not val2.isdigit():
+			raise ValueError('excepts for integer and string, received two strings')
+		else:
+			print "Valid input"
+	except ValueError as ex:
+		print "{0}, {1}".format(type(ex), ex.args)	
+		
+		
+parser = argparse.ArgumentParser(description='This program excepts for integer and string')
+parser.add_argument(help='Integer',dest='val1')
+parser.add_argument(help='String',dest='val2')
+args = parser.parse_args()
+
+except_for_string_int(args.val1,args.val2)
 
