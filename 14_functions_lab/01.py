bccd75ac53d7f023feb032b6ca6dc438a706d1db
HodayaShabtay <hodush12@gmail.com>
2016-07-15 18:19:45 +0300

    Solution for: sol-python-verint.functions-lab/01.py

--- 14_functions_lab/01.py
+++ 14_functions_lab/01.py
@@ -1,7 +1,23 @@
-"""
-Write 2 functions:
-    mysum - returns the sum of its input arguments
-    mymul - returns the multiplication of its input arguments
-    Ignore non-numeric arguments
-"""
 
+import sys
+from operator import mul
+import argparse
+
+def sum_params(*nums):
+	return sum([n for n in nums if type(n)==int])
+	
+	
+def mul_params(*nums):
+	return reduce(mul,[n for n in nums if type(n)==int])
+	
+
+parser = argparse.ArgumentParser(description='This program calculates sum & mul of a given list')
+parser.add_argument(nargs='+',help='Insert list of numbers',dest='numbers')
+args = parser.parse_args()
+	
+	
+print "Sum of [1,2,3,4, hodaya] is {0}".format(sum_params(args.numbers))
+print "Mul of [1,2,3,shabtay,4] is {0}".format(mul_params(args.numbers))
+	
+	
+	
