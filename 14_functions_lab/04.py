60d8da83fa96dbae32f6ceb9eca8cb4916df779a
HodayaShabtay <hodush12@gmail.com>
2016-07-15 18:19:48 +0300

    Solution for: sol-python-verint.functions-lab/04.py

--- 14_functions_lab/04.py
+++ 14_functions_lab/04.py
@@ -1,6 +1,13 @@
-"""
-Write a function that takes minlen and
-a list of words, and returns only the words
-longer than minlen
-"""
 
+import sys
+import os
+
+def length_validation(length, *words):
+	try:
+		return list(word for word in words if len(word)>length)
+	except:
+		print "{0}, {1}".format(sys.exc_info()[0],sys.exc_info()[1])
+	
+	
+print length_validation(3, 'dad', 'book','child','pen')
+print length_validation(3, 'dad', 'book','child','pen',3333)
