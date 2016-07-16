38a8a56297b91ccc6f9fe182e43f61012beb1877
HodayaShabtay <hodush12@gmail.com>
2016-07-17 00:20:34 +0300

    Solution for: sol-python-verint.functions-lab/05.py

--- 14_functions_lab/05.py
+++ 14_functions_lab/05.py
@@ -1,10 +1,15 @@
-"""
-Write a function groupby that takes a list
-and a function and returns a dictionary
-keyd by the return value of the function on the list items
 
-For example:
-    groupby(lambda s: s[0], ['foo', 'fi', 'hello', 'hi'])
-    returns: { 'f': ['foo','fi'], 'h': ['hello', 'hi'] }
-"""
+import sys
+import os
+import collections
 
+def groupby(f, *mylist):
+	dic = collections.defaultdict(list)
+	try:
+		for n in mylist:
+			dic[f(n)].append(n)
+		return dic
+	except:
+		print "{0}, {1}".format(sys.exc_info()[0],sys.exc_info()[1])
+		
+print groupby(lambda s: s[0], 'hello', 'hi', 'help', 'bye', 'hodaya')
